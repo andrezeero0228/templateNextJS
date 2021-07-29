3a1372d8d2d63117fa5ba375185a3d3dae7e8de9
andrezeero0228 <andrezeero0228@hotmail.com>
2021-07-29 01:32:56 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,67 @@
+/* eslint-disable prettier/prettier */
+import { Typography } from '@material-ui/core'
+// import Products from './feature/products/index'
+import { GetStaticProps } from 'next'
+import axios from 'axios'
+import { Person } from 'schema-dts'
 import React from 'react'
-import Head from 'next/head'
-import { Typography } from '@material-ui/core';
+import { JsonLd } from 'react-schemaorg'
 
-const Home: React.FC = () => {
+interface Products {
+  id: number
+  title: string
+  author: string
+}
+
+const Home = ({data}:{data : Products[]}): JSX.Element => {
+  const products = data.map(p => {
+    return (
+      <div key={p.id}>
+            <JsonLd<Person>
+        item={{
+          "@context": "https://schema.org",
+          "@type": "Person",
+          name: p.title,
+          knowsAbout: ["Compilers", "Computer Science"],
+        }}
+        />
+        <li>{p.id}</li>
+        <li>{p.title}</li>
+      </div>
+    )
+  })
   return (
-    <div>
-      <Head>
-        <title>Selos</title>
-      </Head>
-
-      <main >
-        <h1 >
-          CRUD Selooims
-        </h1>
-        <Typography style={{color : "#e3e3e3"}}>blalbalbalbalbla</Typography>
-      </main>
-    </div>
+    <main>
+      <h1>POC google tag manager</h1>
+      <Typography style={{ color: '#e3e3e3' }}>blalbalbalbalbla</Typography>
+      <ul>{products}</ul>
+    </main>
   )
 }
 
 export default Home
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await axios.get<Products[]>('http://localhost:8000/products')
+  console.log(data)
+  return { props: { data }, revalidate: 10 }
+}
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   const { data } = await axios.get<Products[]>('http://localhost:8000/products')
+//   console.log(data)
+//   return { props: { data } }
+// }
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const { data } = await axios.get<Products[]>('http://localhost:8000/products')
+//   console.log(data)
+//   return { props: { data } }
+// }
+
+//todo SSR, Static generation e Incremental generation, getStaticPaths
+
+//useEffect falhou
+//static generation deu certo
+// incremental deu certo
+// server side rendering deu certo
---
+++ src/products/index.ts
@@ -0,0 +1,53 @@
+// import React, {useState } from 'react'
+// import { Thing } from 'schema-dts'
+// import { JsonLd } from 'react-schemaorg'
+
+// const Products: React.FC = () => {
+//   const [count, setCount] = useState(0)
+// //   useEffect(() => {
+// //     renderizaProdutos()
+// //     console.log('oi')
+// //   })
+
+//   const produtos = [
+//     {
+//       type: 'Tipo Um',
+//       name: ['nome um', 'nome dois'],
+//       knowsAbout: ['Compilers', 'Computer Science']
+//     },
+//     {
+//       type: 'Tipo Dois',
+//       name: ['nome tres', 'nome quatro'],
+//       knowsAbout: ['Compilers', 'Computer Science']
+//     },
+//     {
+//       type: 'Tipo Tres',
+//       name: ['nome cinco', 'nome seis'],
+//       knowsAbout: ['Compilers', 'Computer Science']
+//     }
+//   ]
+
+// //   function renderizaProdutos() {
+// //     return produtos.map(p => {
+// //       return (
+// //         <JsonLd<Thing>
+// //           item={{
+// //             '@context': 'https://schema.org',
+// //             '@type': p.type,
+// //             name: p.name,
+// //             knowsAbout: ['Compilers', 'Computer Science']
+// //           }}
+// //         />
+// //       )
+// //     })
+// //   }
+//   return (
+//     <>
+    
+//     </>
+//   )
+// }
+
+// export default Products
+
+export {}
